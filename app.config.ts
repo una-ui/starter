22075cc9b902f89e50916e2089fca7bbc2f7784e
Phojie Rengel <phojrengel@gmail.com>
2023-11-05 01:14:34 +0800

    style: add line comment

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,6 @@
 export default defineAppConfig({
   una: {
+    // you can use any color provided by tailwindcss 💛, or you can define your own colors via uno.config.ts
     primary: 'yellow',
     gray: 'stone'
   }
