b6fef31bad5d35adc270ebe72847f88de200eca4
Phojie Rengel <phojrengel@gmail.com>
2023-11-07 04:37:31 +0800

    chore: initial setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  typescript: {
+    strict: true,
+    typeCheck: true,
+  },
+
+  experimental: {
+    typedPages: true,
+  },
+
   devtools: { enabled: true },
   modules: [
     '@una-ui/nuxt',
