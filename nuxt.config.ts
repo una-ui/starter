afbe712d7a5157e8a4ccfa890070221b518e780a
Phojie Rengel <phojrengel@gmail.com>
2023-11-11 02:22:21 +0800

    chore: update default config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  typescript: {
-    strict: true,
-    typeCheck: true,
-  },
-
-  experimental: {
-    typedPages: true,
-  },
-
-  devtools: { enabled: true },
   modules: [
     '@una-ui/nuxt',
   ],
+
+  // default settings; you can remove this
+  una: {
+    prefix: 'N',
+    themeable: true,
+    global: true,
+  },
 
   colorMode: {
     preference: 'dark',
