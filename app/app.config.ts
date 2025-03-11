5e7bdd774d7187cd05dc10165ce4174f9786d879
Phojie Rengel <phojrengel@gmail.com>
2025-03-12 00:00:02 +0800

    refactor: convert to nuxt v4 compat structure

--- app/app.config.ts
+++ app/app.config.ts
@@ -2,6 +2,7 @@ export default defineAppConfig({
   una: {
     // you can use any color provided by tailwindcss 💛, or you can define your own colors via uno.config.ts
     primary: 'yellow',
-    gray: 'stone'
+    gray: 'stone',
+    radius: 0.5
   }
 })
